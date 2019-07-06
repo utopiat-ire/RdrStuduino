ebec2d0be6f0cece3eda0b9de178c1c0dbe85610
ゆうと <yutopia@utopiat.net>
2019-07-06 11:24:47 +0900

    モーターとセンサー設定をrdrファイルと同名のjsonファイルに追記できるように変更。サンプル多数追加。不具合の修正

--- Program.cs
+++ Program.cs
@@ -40,36 +40,48 @@ namespace Produire.Translator
 			var plugin = PluginManager.Load(typeof(Program).Assembly) as PNamespacePlugin;
 			pluginManager.AddStandard(plugin);
 
+			string rdrName = Path.GetFileNameWithoutExtension(rdrfile);
+			string rdrDir = Path.GetDirectoryName(rdrfile);
 			if (inofile == null)
 			{
-				string inoName = Path.GetFileNameWithoutExtension(rdrfile);
-				string inoDir = Path.Combine(Path.GetDirectoryName(rdrfile), inoName);
+				string inoDir = Path.Combine(rdrDir, rdrName);
 				Directory.CreateDirectory(inoDir);
-				inofile = Path.Combine(inoDir, inoName + ".ino");
+				inofile = Path.Combine(inoDir, rdrName + ".ino");
 			}
 			else
 			{
 			}
 			string inoFN = Path.GetFileName(inofile);
 
-			ScriptParser parser = new ScriptParser(false);
-			ProduireFile rdr = parser.ParseFrom(rdrfile, pluginManager);
-			var compiler = new StuduinoCodeGenerator();
-			if (!compiler.Compile(rdr, inofile))
-			{
-				Console.WriteLine("失敗!");
-				return;
-			}
-
-			//設定
+			//共通設定の読み込み
 			string settingConfig = "settings.json";
 			string json;
 			if (File.Exists(settingConfig))
 				json = File.ReadAllText(settingConfig);
 			else
 				json = "";
-
 			Settings setting = JsonConvert.DeserializeObject<Settings>(json);
+
+			//個別設定の読み込み
+			string settingConfig2 = Path.Combine(rdrDir, rdrName + ".json");
+			if (File.Exists(settingConfig2))
+			{
+				string json2 = File.ReadAllText(settingConfig2);
+				Settings setting2 = JsonConvert.DeserializeObject<Settings>(json2);
+				if (setting2.SensorPort != null) setting.SensorPort = setting2.SensorPort;
+				if (setting2.ServomotorPort != null) setting.ServomotorPort = setting2.ServomotorPort;
+				if (setting2.DCMotorPort != null) setting.DCMotorPort = setting2.DCMotorPort;
+			}
+
+			ScriptParser parser = new ScriptParser(false);
+			ProduireFile rdr = parser.ParseFrom(rdrfile, pluginManager);
+			var compiler = new StuduinoCodeGenerator();
+			compiler.Settings=  setting;
+			if (!compiler.Compile(rdr, inofile))
+			{
+				Console.WriteLine("変換に失敗しました。");
+				return;
+			}
 
 			if (mode != "/trans")
 			{
@@ -88,14 +100,42 @@ namespace Produire.Translator
 				code = code.Replace("{cache_dir}", cache_dir);
 				code = code.Replace("{inoFileName}", inoFN);
 				File.WriteAllText("compile.bat", code, Encoding.Default);
-				var p = Process.Start("compile.bat");
+
+				Process p = new Process();
+				p.StartInfo.UseShellExecute = false;
+				p.StartInfo.RedirectStandardOutput = true;
+				p.StartInfo.RedirectStandardInput = false;
+				p.StartInfo.RedirectStandardError = true;
+				p.StartInfo.CreateNoWindow = true;
+				p.OutputDataReceived += p_OutputDataReceived;
+				p.ErrorDataReceived += P_ErrorDataReceived;
+				p.StartInfo.FileName = "compile.bat";
+
+				p.Start();
+				p.BeginOutputReadLine();
+				p.BeginErrorReadLine();
 				p.WaitForExit();
+
 				Directory.Delete(build_dir, true);
 				Directory.Delete(cache_dir, true);
+
+				Console.WriteLine("完了しました。");
 			}
-			Console.WriteLine("成功しました!");
+			else
+			{
+				Console.WriteLine("成功しました!");
+			}
 		}
 
+		private static void p_OutputDataReceived(object sender, DataReceivedEventArgs e)
+		{
+			Console.WriteLine(e.Data);
+		}
+
+		private static void P_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+		{
+			Console.WriteLine(e.Data);
+		}
 		private static void OutputUsage()
 		{
 			Console.WriteLine("RdrStuduino(プロデルStuduinoツール)");
